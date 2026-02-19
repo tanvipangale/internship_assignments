03ed9afe337c844ea70c7ce29798fe55e9d46fd6
tanvipangale <pangaletanvi2006@gmail.com>
2026-02-19 15:34:47 +0530

    Create basics.php
    Includes Hello World, Echo vs Print, and Constants.

---
+++ Assignment-1/basics.php
@@ -0,0 +1,14 @@
+<?php
+
+// 1. Hello World
+echo "Hello, world!<br><br>";
+
+// 2. Print using echo and print
+echo "This is printed by using 'echo'.<br>";
+print "This is printed by using 'print'.<br><br>";
+
+// 3. Constants in PHP
+echo "This shows the value of PI as it is constant.<br>";
+define("PI", 3.14);
+echo "The value of PI is: " . PI;
+?>
